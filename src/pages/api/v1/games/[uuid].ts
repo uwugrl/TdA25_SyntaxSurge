d3d5ceac7e1cd0a8da0bdd8f0c2352cb3b2b56a5
mldchan <mldkyt@proton.me>
2024-12-01 10:31:34 +0100

    "It works on my machine"

--- src/pages/api/v1/games/[uuid].ts
+++ src/pages/api/v1/games/[uuid].ts
@@ -99,16 +99,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         })
 
         if (!updResult) {
+            await prisma.$disconnect();
             res.status(404).json({error: "Not found"});
             return;
         }
 
         await prisma.$disconnect();
 
-        if (!updResult) {
-            res.status(404).json({error: "Game not found"});
-            return;
-        }
         res.status(200).json({
             uuid: updResult.id,
             name: updResult.name,
