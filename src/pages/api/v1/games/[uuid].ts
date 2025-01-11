f3f591a137ff9a88eac7f0ca2cc7989b43326ab2
エムエルディー <mldkyt@proton.me>
2025-01-11 09:09:00 +0100

    fix: Some codes were invalid, fix forgotten TODOs

--- src/pages/api/v1/games/[uuid].ts
+++ src/pages/api/v1/games/[uuid].ts
@@ -39,7 +39,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             updatedAt: game.updatedAt.toISOString(),
             difficulty: difficulty,
             board: board,
-            gameState: 'unknown' // FIXME
+            gameState: determineGameState(board)
         });
     } else if (req.method == "PUT") {
         if (!req.query.uuid) {
