0755553e9cadc8d0688f16ad8165bbe067fcceed
Lunya <mldkyt@proton.me>
2025-02-26 18:42:27 +0100

    feat: Search, sort by user list

--- src/pages/accounts.tsx
+++ src/pages/accounts.tsx
@@ -2,11 +2,12 @@
 import React from "react";
 import Header from "@/components/Header";
 import Metadata from "@/components/Metadata";
-import { Button, Card, Link, Stack, Typography } from "@mui/joy";
+import { Button, Dropdown, Input, Link, ListDivider, Menu, MenuButton, MenuItem, Stack, Table } from "@mui/joy";
 import { PrismaClient } from "@prisma/client";
 import { InferGetServerSidePropsType } from "next";
 import { useState } from "react";
 import Pagination from '@/components/Pagination';
+import { ArrowDropDown, Search } from "@mui/icons-material";
 
 
 export async function getServerSideProps() {
@@ -34,6 +35,35 @@ export default function Profile(props: InferGetServerSidePropsType<typeof getSer
     const [page, setPage] = useState(1);
     const lastPage = Math.ceil(props.users.length / 10);
 
+    const SORT_BY = [
+        {
+            id: 'eloDesc',
+            val: 'ELO sestupně'
+        },
+        {
+            id: 'eloAsc',
+            val: 'ELO vzestupně'
+        }
+    ]
+
+    const [sortBy, setSortBy] = useState("");
+    const [fullText, setFullText] = useState("");
+
+    let users = props.users;
+
+    if (sortBy === "eloDesc") {
+        users = users.sort((a, b) => b.elo - a.elo);
+    }
+    if (sortBy === "eloAsc") {
+        users = users.sort((a, b) => a.elo - b.elo);
+    }
+
+    if (fullText.length > 0) {
+        users = users.filter(x => x.name.toLowerCase().includes(fullText.toLowerCase()));
+    }
+
+    users = users.slice((page - 1) * 10, page * 10);
+
     return <>
         <Metadata title={'List účtů'} description={'List účtů ve službě Think different Academy.'} />
         <main className={`w-3/4 m-auto`}>
@@ -43,21 +73,52 @@ export default function Profile(props: InferGetServerSidePropsType<typeof getSer
             <br />
             <br />
 
-
             <Stack gap={1}>
                 <Pagination {...props} page={page} lastPage={lastPage} setPage={x => setPage(x)} />
-                {props.users
-                    .slice((page - 1) * 10, page * 10)
-                    .map((x, i) => (
-                        <Card key={i}>
-                            <Typography level='h2'>{x.name}</Typography>
-                            <Link href={`/account/${x.uuid}`}>
-                                <Button variant="outlined" color="neutral">
-                                    Zobrazit profil
-                                </Button>
-                            </Link>
-                        </Card>
-                    ))}
+
+                <Stack direction="row" gap={1}>
+                    <Dropdown>
+                        <MenuButton endDecorator={<ArrowDropDown />}>{sortBy ? SORT_BY.find(x => x.id === sortBy)?.val : 'Seřadit podle'}</MenuButton>
+                        <Menu>
+                            {sortBy && <>
+                                <MenuItem onClick={() => setSortBy("")}>
+                                    Odstranit seřazení
+                                </MenuItem>
+                                <ListDivider />
+                            </>}
+                            
+                            {SORT_BY.map(x => (
+                                <MenuItem key={x.id} onClick={() => setSortBy(x.id)}>{x.val}</MenuItem>
+                            ))}
+                        </Menu>
+                    </Dropdown>
+            
+                <Input value={fullText} onChange={e => setFullText(e.target.value)} placeholder="Hledat..." startDecorator={<Search />} />
+                </Stack>
+
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>Název</th>
+                            <th>Akce</th>
+                            <th>ELO</th>
+                            <th>W/D/L</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map(x => (
+                            <tr key={x.uuid}>
+                                <td>{x.name}</td>
+                                <td>
+                                    <Link href={`/accounts/${x.uuid}`}><Button variant="plain">Zobrazit uživatele</Button></Link>
+                                </td>
+                                <td>{x.elo}</td>
+                                <td>{`${x.wins}/${x.draws}/${x.losses}`}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                    
+                </Table>
                 <Pagination {...props} page={page} lastPage={lastPage} setPage={x => setPage(x)} />
             </Stack>
 
